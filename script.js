b24d19dba601b79dcfe8387c9c0e68bb52fd8c7b
CarrascoOliva <145786543+CarrascoOliva@users.noreply.github.com>
2024-05-17 09:34:18 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const images = document.querySelectorAll('.gallery img');
 const countdownContainer = document.getElementById('countdown');
-const endDate = new Date('2024-04-18T23:45:00'); // Asegúrate de ajustar el año
+const endDate = new Date('2024-05-17T11:20:00'); // Asegúrate de ajustar el año
 
 
 let currentIndex = 0;
